e5bca9aad319501065a71b695c5a252996e3726b
jhoaking <peredoroj@gmail.com>
2025-04-01 13:38:33 -0400

    haciendo api de gesion de libros y creando archivos para testing

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import { createPool } from "mysql2/promise";
+
+import { DB_HOST,DB_PASSWORD,DB_PORT,DB_USER,DB } from "./config.js";
+
+export const connection = createPool({
+    port : DB_PORT,
+    user : DB_USER,
+    host : DB_HOST,
+    password : DB_PASSWORD,
+    database : DB
+})
